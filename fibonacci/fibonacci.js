4a66a90abc7d4f954fe50eb9d8e107726443add3
Steve Lally <steve.lally89@gmail.com>
2019-09-22 16:25:38 -0400

    add personal fibonacci solution

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof num != "number"){
+        num = Number(num);
+    }
 
+    if (num < 0){
+        return 'OOPS';
+    } else if (num == 0){
+        return 0;
+    }
+
+    let fibArr = [1, 1];
+
+    while (fibArr.length < num){
+        let first = fibArr[fibArr.length - 1];
+        let second = fibArr[fibArr.length - 2];
+        fibArr.push(first + second);
+    }
+
+    return fibArr[fibArr.length - 1];
 }
 
 module.exports = fibonacci
